56b88c3e6fba36642e15ff95b71f6b5bdc10aaa8
Sammy Kumar <sammykumardemo@gmail.com>
2026-08-10 01:23:08 +0530

    New Test File added

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -8,7 +8,17 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link @Dev', async ({ page }) => {
+/* test('get started link @Dev', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+}); */
+
+test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
